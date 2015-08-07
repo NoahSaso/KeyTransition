016f0cb96a85e7406f0ebeb8fce7fda14764fce8
Sassoty <sassoty@gmail.com>
2015-08-06 18:10:13 -0700

    Added animation icon to top

---
+++ Preferences/KTCustomListItemsController.mm
@@ -0,0 +1,14 @@
+#import <Preferences/Preferences.h>
+#import "global.h"
+
+@interface KTCustomListItemsController : PSListItemsController
+@end
+
+@implementation KTCustomListItemsController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Animation" inBundle:KTBundle]];
+}
+
+@end
