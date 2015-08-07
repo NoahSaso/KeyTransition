54b7b616845becce6a004c8be8872f186f1d3c4e
Sassoty <sassoty@gmail.com>
2015-08-06 20:07:38 -0700

    Added rick roll lol

--- Preferences/KeyTransition.mm
+++ Preferences/KeyTransition.mm
@@ -50,6 +50,21 @@
 	    	self.navigationItem.titleView.alpha = 1;
 	    }];
 	});
+	// Might be an easter egg, might not ;)
+	UIRefreshControl* refreshControl = [[UIRefreshControl alloc] init];
+	[refreshControl addTarget:self action:@selector(refreshOrMaybeNotIDontEvenKnowWhatThisDoesLol:) forControlEvents:UIControlEventValueChanged];
+	refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Congratulations, you found me! Keep dragging for a surprise ;)" attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14],NSForegroundColorAttributeName:[UIColor blackColor]}];
+	[tableView addSubview:refreshControl];
+	// Hopefully fixes a weird text overlap bug
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [refreshControl beginRefreshing];
+        [refreshControl endRefreshing];
+    });
+}
+
+- (void)refreshOrMaybeNotIDontEvenKnowWhatThisDoesLol:(UIRefreshControl *)refreshControl {
+	[refreshControl endRefreshing];
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.youtube.com/watch?v=dQw4w9WgXcQ"]];
 }
 
 - (void)tweet {
